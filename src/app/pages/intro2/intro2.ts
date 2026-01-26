4cfcd1f9a29147d4f85303395b91151c0780e577
Alejandro Aranda <Alejandro.Aranda2@alu.uclm.es>
2026-01-26 21:35:26 +0100

    Intro2 Finished, time to start with minigames :)

--- src/app/pages/intro2/intro2.ts
+++ src/app/pages/intro2/intro2.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { GAME_STEPS } from '../../core/game-flow';
 
 import { GameMenu } from '../../core/game-menu/game-menu';
 
@@ -13,9 +12,31 @@ import { GameMenu } from '../../core/game-menu/game-menu';
 export class Intro2 {
 
   currentRoute = ''
+  progress = signal(0)
+
+  // Necessary to clear setInterval 
+  private destroyRef = inject(DestroyRef);
+
 
   constructor(private router: Router){
+
     this.currentRoute = this.router.url
+    const intervalId = setInterval(() => {
+      if (this.progress() < 100) {
+        this.progress.update(v => v - (Math.min(1, v/10)));
+      }
+    }, 100);
+
+      this.destroyRef.onDestroy(() => {
+    clearInterval(intervalId);
+    });
+  }
+
+  
+  fill() {
+    if (this.progress() < 100) {
+      this.progress.update(value => value + 5);
+    }
   }
 
 
